f19977bd73d489f22c2b75a8cb07f3f2f3bb99b5
Artur Yaroshinski <62154380+arturyaroshinski@users.noreply.github.com>
2021-03-04 16:01:35 +0300

    feat: complete transform array task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let moveCount = Math.pow(2, disksNumber) - 1;
+  let sec = turnsSpeed / 3600;
+  let res = moveCount / sec;
+
+  return { turns: moveCount, seconds: Math.trunc(res) };
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+
+  let myArr = [...arr];
+
+  for (let i = 0; i < myArr.length; i++) {
+    switch (myArr[i]) {
+      case "--discard-next":
+        if (i + 1 == myArr.length) {
+          break;
+        } else {
+          myArr.splice(i + 1, 1);
+        }
+
+        break;
+      case "--discard-prev":
+        if (i == 0) {
+          break;
+        } else {
+          myArr.splice(i - 1, 1);
+        }
+
+        break;
+      case "--double-next":
+        if (arr[i + 1] != myArr.length) {
+          myArr[i] = myArr[i + 1];
+        }
+
+        break;
+      case "--double-prev":
+        if (myArr[i] != 0) {
+          myArr[i] = myArr[i - 1];
+        }
+
+        break;
+      default:
+    }
+  }
+
+  let result = [];
+
+  myArr.forEach((val) => {
+    if (
+      typeof val !== "undefined" &&
+      val !== "--discard-next" &&
+      val !== "--discard-prev" &&
+      val !== "--double-next" &&
+      val !== "--double-prev"
+    )
+      result.push(val);
+  });
+
+  return result;
 };
